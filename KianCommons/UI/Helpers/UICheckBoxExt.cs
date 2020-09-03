5cfb5017d6ff7d2cabbfba5a86c95027fa7b1018
kian.zarrin <kian.zarrin@gmail.com>
2020-09-03 20:53:40 +0300

    check box size changed. no longer equals to parent. also heigh is shrink to exactly fit checkbox

--- KianCommons/UI/Helpers/UICheckBoxExt.cs
+++ KianCommons/UI/Helpers/UICheckBoxExt.cs
@@ -7,12 +7,14 @@ namespace KianCommons.UI {
         public override void Awake() {
             base.Awake();
             name = nameof(UICheckBoxExt);
-            height = 30f;
-            clipChildren = true;
+            height = 20;
+            width = 100;
+
+            //clipChildren = true;
 
             UISprite sprite = AddUIComponent<UISprite>();
             sprite.spriteName = "ToggleBase";
-            sprite.size = new Vector2(19f, 19f);
+            sprite.size = new Vector2(height, height);
             sprite.relativePosition = new Vector2(0, (height - sprite.height) / 2);
 
             checkedBoxObject = sprite.AddUIComponent<UISprite>();
@@ -25,8 +27,10 @@ namespace KianCommons.UI {
             label.textScale = 0.9f;
             label.relativePosition = new Vector2(sprite.width + 5f, (height - label.height) / 2 + 1);
 
-            eventCheckChanged += (component, value) => OnCheckChanged();
+            eventCheckChanged += (_,__) => OnCheckedChanged();
         }
+
+        public virtual void OnCheckedChanged() { Invalidate(); }
 
         public virtual string Label {
             get => label.text;
@@ -42,11 +46,6 @@ namespace KianCommons.UI {
                 tooltip = value;
                 RefreshTooltip();
             }
-        }
-
-        public override void Start() {
-            base.Start();
-            width = parent.width;
         }
     }
 }
