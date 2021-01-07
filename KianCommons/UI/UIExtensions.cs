241c6c1ffb7572587fe82103e1d975188031bc72
kian.zarrin <kian.zarrin@gmail.com>
2021-01-07 20:27:37 +0200

    polish

--- KianCommons/UI/UIExtensions.cs
+++ KianCommons/UI/UIExtensions.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using ColossalFramework.UI;
 using UnityEngine;
+using UnityEngine.UI;
 using KianCommons;
 
 namespace KianCommons.UI {
     internal static class UIExtensions {
         public static T AddUIComponent<T>(this UIView view) where T: UIComponent {
             return view.AddUIComponent(typeof(T)) as T;
+        }
+
+        public static void DestroyFull(this UIComponent c) {
+            c.SetAllDeclaredFieldsToNull();
+            GameObject.Destroy(c.gameObject);
         }
     }
 }
