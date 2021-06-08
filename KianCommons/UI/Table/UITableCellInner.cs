0778d3697348a4209c0f5e50d649491097115f0d
xlf1024 <33392024+xlf1024@users.noreply.github.com>
2021-06-08 15:15:47 +0200

    Replace the try...catch with ?.

--- KianCommons/UI/Table/UITableCellInner.cs
+++ KianCommons/UI/Table/UITableCellInner.cs
@@ -15,14 +15,14 @@ namespace KianCommons.UI.Table {
             name = GetType().Name;
             atlas = TextureUtil.Ingame;
             eventSizeChanged += (_, _) => {
-                try { // the first size change occurs within UITableCellOuter::Activate, at which point UITableCellOuter.parent isn't a UITableRow yet.
+                //try { // the first size change occurs within UITableCellOuter::Activate, at which point UITableCellOuter.parent isn't a UITableRow yet.
                     var _outerCell = parent as UITableCellOuter;
-                    var _row = _outerCell.parent as UITableRow;
-                    var _table = _row.parent as UITable;
-                    _table.FitColumn(_outerCell.ColumnIndex);
-                }catch(NullReferenceException _) {
+                    var _row = _outerCell?.parent as UITableRow;
+                    var _table = _row?.parent as UITable;
+                    _table?.FitColumn(_outerCell.ColumnIndex);
+                //}catch(NullReferenceException _) {
 
-                }
+                //}
             };
         }
     }
