777bb51707437ba0758a9fd2c27236ac752f5ba1
kian.zarrin <kian.zarrin@gmail.com>
2021-06-09 18:30:24 +0300

    polish

--- KianCommons/Util/EnumBitMaskExtensions.cs
+++ KianCommons/Util/EnumBitMaskExtensions.cs
@@ -55,13 +55,15 @@ namespace KianCommons {
             if (!Enum.GetUnderlyingType(type).IsInteger())
                 throw new Exception($"Type '{type.FullName}' is not integer based enum.");
         }
-
+        internal static bool IsFlagSet(this NetInfo.Direction value, NetInfo.Direction flag) => (value & flag) != 0;
         internal static bool IsFlagSet(this NetInfo.LaneType value, NetInfo.LaneType flag) => (value & flag) != 0;
         internal static bool IsFlagSet(this VehicleInfo.VehicleType value, VehicleInfo.VehicleType flag) => (value & flag) != 0;
         internal static bool IsFlagSet(this NetNode.Flags value, NetNode.Flags flag) => (value & flag) != 0;
         internal static bool IsFlagSet(this NetSegment.Flags value, NetSegment.Flags flag) => (value & flag) != 0;
         internal static bool IsFlagSet(this NetLane.Flags value, NetLane.Flags flag) => (value & flag) != 0;
 
+        internal static bool CheckFlags(this NetInfo.Direction value, NetInfo.Direction required, NetInfo.Direction forbidden = 0) =>
+            (value & (required | forbidden)) == required;
         internal static bool CheckFlags(this NetInfo.LaneType value, NetInfo.LaneType required, NetInfo.LaneType forbidden = 0) =>
             (value & (required | forbidden)) == required;
         internal static bool CheckFlags(this VehicleInfo.VehicleType value, VehicleInfo.VehicleType required, VehicleInfo.VehicleType forbidden = 0) =>
@@ -70,8 +72,9 @@ namespace KianCommons {
             (value & (required | forbidden)) == required;
         internal static bool CheckFlags(this NetSegment.Flags value, NetSegment.Flags required, NetSegment.Flags forbidden=0) =>
             (value & (required | forbidden)) == required;
-        internal static bool CheckFlags(this NetLane.Flags value, NetLane.Flags required, NetLane.Flags forbidden=0) =>
+        internal static bool CheckFlags(this NetLane.Flags value, NetLane.Flags required, NetLane.Flags forbidden = 0) =>
             (value & (required | forbidden)) == required;
+
 
         public static bool CheckFlags<T>(this T value, T required, T forbidden)
             where T : struct, Enum, IConvertible {
