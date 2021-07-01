421c95357d5f252ba796339af0d0efd7b0bdca4f
kianzzarrin <kian.zarrin@gmail.com>
2021-07-01 18:51:02 +0300

    fit to screen extension

--- KianCommons/UI/UIExtensions.cs
+++ KianCommons/UI/UIExtensions.cs
@@ -1,16 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using ColossalFramework.UI;
-using UnityEngine;
-using UnityEngine.UI;
-using KianCommons;
 using System.Runtime.CompilerServices;
+using UnityEngine;
 
 namespace KianCommons.UI {
     internal static class UIExtensions {
-        public static T AddUIComponent<T>(this UIView view) where T: UIComponent {
+        public static void FitToScreen(this UIComponent target) {
+            Log.Called("target=" + target);
+            Vector2 resolution = target.GetUIView().GetScreenResolution();
+            target.absolutePosition = new Vector2(
+                Mathf.Clamp(target.absolutePosition.x, 0, resolution.x - target.width),
+                Mathf.Clamp(target.absolutePosition.y, 0, resolution.y - target.height));
+            Log.Info($"target.absolutePosition={target.absolutePosition}, resolution={resolution}");
+        }
+
+        public static T AddUIComponent<T>(this UIView view) where T : UIComponent {
             return view.AddUIComponent(typeof(T)) as T;
         }
 
