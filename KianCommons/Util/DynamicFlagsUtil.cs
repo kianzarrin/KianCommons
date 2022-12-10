050fc086f707f334ee7d0886a197a3f74becce16
kianzzarrin <kian.zarrin@gmail.com>
2022-12-10 23:41:59 +0200

    IsAnyFlagSetOrEmpty

--- KianCommons/Util/DynamicFlagsUtil.cs
+++ KianCommons/Util/DynamicFlagsUtil.cs
@@ -1,21 +1,22 @@
-namespace KianCommons {
-    using System;
+namespace KianCommons; 
+public static class DynamicFlagsUtil {
+    public readonly static ulong[] EMPTY_FLAGS = new ulong[0];
+    public readonly static string[] EMPTY_TAGS = new string[0];
+    public readonly static DynamicFlags NONE = new DynamicFlags(EMPTY_FLAGS);
 
-    public static class DynamicFlagsUtil {
-        public readonly static ulong[] EMPTY_FLAGS = new ulong[0];
-        public readonly static string[] EMPTY_TAGS = new string[0];
-        public readonly static DynamicFlags NONE = new DynamicFlags(EMPTY_FLAGS);
+    public static bool CheckFlags(this DynamicFlags flags, DynamicFlags required, DynamicFlags forbidden) =>
+        DynamicFlags.Check(flags, required: required, forbidden: forbidden);
 
-        public static bool CheckFlags(this DynamicFlags flags, DynamicFlags required, DynamicFlags forbidden) =>
-            DynamicFlags.Check(flags, required: required, forbidden: forbidden);
+    public static bool IsAnyFlagSet(this DynamicFlags flags, DynamicFlags flags2) =>
+        !DynamicFlags.Check(flags, required: new DynamicFlags(EMPTY_FLAGS), forbidden: flags2);
 
-        public static bool IsAnyFlagSet(this DynamicFlags flags, DynamicFlags flags2) =>
-            !DynamicFlags.Check(flags, required: new DynamicFlags(EMPTY_FLAGS), forbidden: flags2);
+    public static bool IsAnyFlagSetOrEmpty(this DynamicFlags flags, DynamicFlags flags2) =>
+    (flags.IsEmpty && flags2.IsEmpty) || flags.IsAnyFlagSet(flags2);
 
-        public static bool CheckFlags(this DynamicFlags2 flags, DynamicFlags2 required, DynamicFlags2 forbidden) =>
-            DynamicFlags2.CheckFlags(flags, required: required, forbidden: forbidden);
 
-        public static bool IsAnyFlagSet(this DynamicFlags2 flags, DynamicFlags2 flags2) =>
-            !DynamicFlags2.IsAnyFlagSet(flags, flags2);
-    }
+    public static bool CheckFlags(this DynamicFlags2 flags, DynamicFlags2 required, DynamicFlags2 forbidden) =>
+        DynamicFlags2.CheckFlags(flags, required: required, forbidden: forbidden);
+
+    public static bool IsAnyFlagSet(this DynamicFlags2 flags, DynamicFlags2 flags2) =>
+        !DynamicFlags2.IsAnyFlagSet(flags, flags2);
 }
